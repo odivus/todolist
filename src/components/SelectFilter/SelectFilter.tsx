d004060052dcc8eec2d813ff71ac6bd41cc67942
Oleg Petko <odivus@gmail.com>
2025-03-02 13:33:47 +0300

    Modify filter priority

--- src/components/SelectFilter/SelectFilter.tsx
+++ src/components/SelectFilter/SelectFilter.tsx
@@ -1,12 +1,29 @@
 import React, { useState } from 'react';
 import SelectPriority from '../SelectPriority/SelectPriority';
+import Select from '../Ui/Select/Select'
 
 const SelectFilter: React.FC = () => {
+  const [priority, setPriority] = useState<'any'|'low' | 'medium' | 'high'>('any')
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setPriority(e.target.value as 'any'|'low' | 'medium' | 'high');
+  }
+
+  const options = {
+    any: 'Любой',
+    low: 'Низкий',
+    medium: 'Средний',
+    high: 'Высокий',
+  }
+
   return (
     <div>
       <h5>Фильтровать</h5>
       <h6>По&nbsp;приоритету:</h6>
-      <SelectPriority />
+      <Select
+        value={priority}
+        options={options}
+        handleChange={handleChange}
+      />
     </div>
   );
 }
--- src/components/SelectPriority/SelectPriority.tsx
+++ src/components/SelectPriority/SelectPriority.tsx
@@ -2,13 +2,12 @@ import React, { useState } from 'react';
 import Select from '../Ui/Select/Select';
 
 const SelectPriority: React.FC = () => {
-  const [priority, setPriority] = useState<'any'|'low' | 'medium' | 'high'>('any')
+  const [priority, setPriority] = useState<'low' | 'medium' | 'high'>('low')
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setPriority(e.target.value as 'any'|'low' | 'medium' | 'high');
+    setPriority(e.target.value as 'low' | 'medium' | 'high');
   }
 
   const options = {
-    any: 'Любой',
     low: 'Низкий',
     medium: 'Средний',
     high: 'Высокий',
