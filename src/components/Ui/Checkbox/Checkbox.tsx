e89de37fdf0c98d5bbc5347fc04ae6f72b9362b3
Oleg Petko <odivus@gmail.com>
2025-03-02 18:04:16 +0300

    Edit checkbox style

--- src/components/Ui/Checkbox/Checkbox.tsx
+++ src/components/Ui/Checkbox/Checkbox.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Checkbox: React.FC = () => {
   return (
     <label>
-      <input type='checkbox' name='checkbox' />
+      <input type='checkbox' name='checkbox' style={{ cursor: 'pointer' }} />
     </label>
   );
 }
