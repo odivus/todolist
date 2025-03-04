172260bfa06d37d1e2dbc95977396da8af2abf85
Oleg Petko <odivus@gmail.com>
2025-03-04 19:15:05 +0300

    Fix index types

--- src/index.tsx
+++ src/index.tsx
@@ -30,8 +30,7 @@ const App: React.FC = () => (
   </>
 );
 
-// @ts-ignore
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
